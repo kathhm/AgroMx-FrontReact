bd181ac49d30749b810cb559b465eaec4fefc101
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-22 19:09:18 -0600

    add: fonts mako and gabriela

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import '@fontsource/gabriela'
+import '@fontsource/mako'
 
 function App() {
 
