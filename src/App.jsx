1b14b2796a955b2d77f4e9efb8dd5f542bb4ee9b
Lisset Sánchez <131899300+Lisset100@users.noreply.github.com>
2025-02-23 20:17:30 -0600

    feat: se agregó footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
 import '@fontsource/gabriela'
 import '@fontsource/mako'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
