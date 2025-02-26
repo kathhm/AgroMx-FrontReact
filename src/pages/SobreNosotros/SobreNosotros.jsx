9ce692dce9aa089890c857f2af0a49c45f384d51
Thania Arredondo <elizabethania.te@gmail.com>
2025-02-25 18:43:11 -0600

    mision y vision

--- src/pages/SobreNosotros/SobreNosotros.jsx
+++ src/pages/SobreNosotros/SobreNosotros.jsx
@@ -11,21 +11,23 @@ function SobreNosotros () {
 
                     <div className='container-fluid'>
                         <div className='row'>
-                            <div className='col-2'></div>
-                            <div className='col-4 col-lg-2'>
+                            <div className='col-2 col-lg-2'></div>
+                            <div className='col-4 col-lg-2 col-md-2 text-center'>
                             <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-3.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
                             </div>
-                            <div className='col-4 col-lg-2'>
-                            <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-3.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
+                            <div className='col-4 col-lg-2 col-md-2 text-center'>
+                            <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-012.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
                             </div>
-                            <div className='col-4 col-lg-2'>
-                            <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-3.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
+                            <div className='col-2 col-sm-2 d-lg-none d-md-none'></div>
+                            <div className='col-2 col-sm-2 d-lg-none d-md-none'></div>
+                            <div className='col-4 col-lg-2 col-md-2 text-center'>
+                            <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-13.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
                             </div>
-                            <div className='col-4 col-lg-2'>
-                            <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-3.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
+                            <div className='col-4 col-lg-2 col-md-2 text-center'>
+                            <img className='imagen-ods' src="/public/images/Sustainable_Development_Goal-es-15.jpg" alt="Imagen de Objetivo de Desarrollo Sostenible 3: Salud y Bienestar"/>
                             </div>
 
-                            <div className='col-2'></div>
+                            <div className='col-2  col-lg-2'></div>
                         </div>
                     </div>
 
@@ -33,6 +35,59 @@ function SobreNosotros () {
                     <p className='parrafo-verde text-center'> Juntos, impulsamos un sistema alimentario más justo y sostenible.</p>
                 </div>
             </main>
+
+            <section>
+                <div className='bloque-azul'>
+                    <div className='container-fluid'>
+                        <div className='row'>
+                            <div className='col-6 border text-center'>
+                                <h2>Misión</h2>
+                                <div className='row'>
+                                    <div className='col-12 border'>
+                                        <p>Conectar productos agroecológicos con consumidores interesados en mejorar su alimentación, fomentar el comercio y economía locales y cuidar el medio ambiente.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className='col-6 border text-center'>
+                                <img className='mision' src="public/images/mision.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-6  text-center'>
+                                <img className='vision' src="/public/images/vision.png" alt="" />
+                            </div>
+                            <div className='col-6 border text-center'>
+                                <h2>Visión</h2>
+                                <div className='row'>
+                                    <div className='col-12 border text-end'>
+                                        <p>Ser líderes en la distribución de alimentos agroecológicos, creando una comunidad comprometida con una alimentación sana y sostenible. 
+                                        </p>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+               {/*  <div class="container">
+    <div class="row">
+        <div class="col-xs-6">
+            <div class="big-box">image</div>
+        </div>
+        <div class="col-xs-6">
+            <div class="row">
+                <div class="col-xs-6"><div class="mini-box">1</div></div>
+                <div class="col-xs-6"><div class="mini-box">2</div></div>
+                <div class="col-xs-6"><div class="mini-box">3</div></div>
+                <div class="col-xs-6"><div class="mini-box">4</div></div>
+            </div>
+        </div>
+    </div>
+</div> */}
+{/* <div class="row"> <div class="col-sm-9"> Level 1: .col-sm-9 <div class="row"> <div class="col-8 col-sm-6"> Level 2: .col-8 .col-sm-6 </div> <div class="col-4 col-sm-6"> Level 2: .col-4 .col-sm-6 </div> </div> </div> </div> */}
+            </section>
         
         
         </>
