2ab9713036a815f9bf304afa6e2195b4daad2e07
DavidAvellaneda97 <git@github.com:DavidAvellaneda97/Entregables-CH50.git>
2025-02-26 22:46:17 -0600

    login un poco estilizado

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,20 +1,32 @@
-function Login() {
+
+import "./login.css"
+
+function login() {
   return (
+    // <div>
     <main>
-        <div className='container-fluid'>
+        <div className='container login-card p-5 text-center mt-5 mx-5'>
             <h1 className='titulo'>inicia sesión</h1> <hr/>
-            <div className='container-fluid'>
-                <form className='form'>
-                    <label htmlFor="correo">correo electronico:</label>
-                    <input type="text" placeholder="example@dominio.com" id="correo"/><br/>
-                    <label htmlFor="contraseña">contraseña</label>
-                    <input type="password" placeholder="introduce tu contraseña" id="contraseña"/>
-                    <button type="submit">iniciar sesión</button>
+            <div className='container-fluid justify-content-center d-flex'>
+                <form className='form col-8 row'>
+                    <label className="etiqueta me-2 form-label text-align-start" for="correo">correo electronico:</label>
+                    <input className="form-control" type="text" placeholder="example@dominio.com" id="correo"/><br/>
+                    <label className="etiqueta me-2 form-label text-align-start" for="contraseña">contraseña</label>
+                    <input className="form-control" type="password" placeholder="introduce tu contraseña" id="contraseña"/>
+                    <button className="btn custom-btn w-50 mt-5" type="submit">iniciar sesión</button>
+
+
                 </form>
+                
             </div>
+
         </div>
-    </main>
-  )
+        
+
+      
+    {/* </div> */}
+  </main>
+)
 }
 
-export default Login
+export default login
