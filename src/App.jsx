f8f5d6aac215ab2f88d8e9971cf34ebb4f7dba56
Lisset Sánchez <131899300+Lisset100@users.noreply.github.com>
2025-02-28 17:17:19 -0600

    DetallesProductos

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import '@fontsource/mako'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Catalogo from './pages/Catalogo/Catalogo'
+import DetallesProducto from './pages/DetallesProducto/DetallesProducto'
+import NuevosProductos from './pages/Productos/NuevosProductos'
 import { SobreNosotros } from './pages/SobreNosotros/SobreNosotros'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NuevosProductos from './pages/Productos/NuevosProductos';
-import Catalogo from './pages/Catalogo/Catalogo';
 
-import Inicio from './pages/Inicio';
+import Inicio from './pages/Inicio'
 
 
 
@@ -24,16 +25,17 @@ function App() {
         <Header></Header>
         {/* <Inicio></Inicio> */}
         <Routes>
-          <Route path="/SobreNosotros" element = {<SobreNosotros/>} />
-          <Route path="/Inicio" element = {<Inicio/>}></Route>
-          <Route path="/" element = {<Inicio/>}></Route>
-          <Route path="/NuevosProductos" element = {<NuevosProductos/>}></Route>
+          <Route path="/SobreNosotros" element={<SobreNosotros />} />
+          <Route path="/Inicio" element={<Inicio />}></Route>
+          <Route path="/" element={<Inicio />}></Route>
+          <Route path="/NuevosProductos" element={<NuevosProductos />}></Route>
           <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/DetallesProducto" element={<DetallesProducto />} />
 
         </Routes>
 
         <Footer></Footer>
-        </BrowserRouter>
+      </BrowserRouter>
     </>
   )
 }
