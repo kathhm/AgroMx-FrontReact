200b026585aa3e1239187509f0a1deb0a3e3071e
DavidAvellaneda97 <git@github.com:DavidAvellaneda97/Entregables-CH50.git>
2025-02-28 16:00:55 -0600

    arreglos a login 28-02

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,7 +5,7 @@ function login() {
   return (
     // <div>
     <main>
-        <div className='container login-card p-5 text-center mt-5 mx-5'>
+        <div className='container login-card m-2 p-5 text-center mt-5'>
             <h1 className='titulo'>inicia sesión</h1> <hr/>
             <div className='container-fluid justify-content-center d-flex'>
                 <form className='form col-8 row'>
@@ -13,7 +13,7 @@ function login() {
                     <input className="form-control" type="text" placeholder="example@dominio.com" id="correo"/><br/>
                     <label className="etiqueta me-2 form-label text-align-start" for="contraseña">contraseña</label>
                     <input className="form-control" type="password" placeholder="introduce tu contraseña" id="contraseña"/>
-                    <button className="btn custom-btn w-50 mt-5" type="submit">iniciar sesión</button>
+                    <button className="btn custom-btn mt-5" type="submit">iniciar sesión</button>
 
 
                 </form>
