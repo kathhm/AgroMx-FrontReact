c4335180cf3b82513227e7a740bcce7f262b46a4
Katherine Hinojosa <geofisikatt@gmail.com>
2025-03-01 23:23:44 -0600

    imagenes y texto descriptivo para el carrusel

--- src/pages/Comunidad/Carrusel.jsx
+++ src/pages/Comunidad/Carrusel.jsx
@@ -3,44 +3,50 @@ import "./Carrusel.css"; // Importa estilos personalizados
 
 const Carrusel = () => {
   return (
-    <div className="carousel-container">
+<div className="carousel-container d-flex">
       <div id="customCarousel" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          {/* Slide 1 - IMAGEN PRINCIPAL */}
-          <div className="carousel-item active">
+        <div className="carousel-inner d-flex">
+          {/* Slide 1 - Imagen principal */}
+          <div className="carousel-item active carousel-main">
             <div className="slide-content slide-bg-main">
               <img
-                src="https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg"
-                className="carousel-image large"
+                src="./public/images/milpa-alta.png"
+                className="carousel-image"
                 alt="Imagen principal"
               />
+              <a className="subtitulo" href="https://gatoencerrado.news/2023/02/01/milpa-alta-el-territorio-campesino-de-cdmx-que-resiste-en-solidaridad-y-comercio-justo/"><h3 className="subtitulo">Milpa Alta, el territorio campesino de CDMX que resiste en solidaridad y comercio justo.</h3></a>
               <p className="slide-text">
-                En las afueras de la CDMX, Don Manuel ha dedicado más de 20 años a la agricultura, pero hace cinco decidió transformar su cultivo en un huerto agroecológico. “Me di cuenta de que la tierra necesitaba descanso y respeto”.
+                La producción agroecológica en el suelo de conservación de la Ciudad de México es una actividad realizada por familias y cooperativas que habitan principalmente las alcaldías de la región sureste de la metrópolis, quienes participan en mercados verdes y espacios de venta alternativos. Dentro de este escenario, una iniciativa de comercio justo ha destacado por ser también una red solidaria que promueve prácticas agroecológicas y restaurativas ambiental y socialmente.
               </p>
             </div>
           </div>
 
-          {/* Slide 2 - IMAGEN LATERAL IZQUIERDA*/}
-          <div className="carousel-item">
+          {/* Slide 2 - Imagen lateral izquierda */}
+          <div className="carousel-item carousel-left">
             <div className="slide-content slide-bg-left">
               <img
-                src="https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg"
-                className="carousel-image small"
+                src="./public/images/romeritos.jpg"
+                className="carousel-image"
                 alt="Imagen lateral izquierda"
               />
-              <p className="slide-text small-text">Lorem ipsum dolor sit amet...</p>
+              <a className="subtitulo" href="https://www.elgrafico.mx/historias/2023/12/24/productores-agricolas-pina-de-romeritos-hablan-del-tradicional-alimento-sembrado-en-la/"><h3 className="subtitulo">Romeritos de Xochimilco, una tradición que llega hasta la mesa navideña</h3></a>
+              <p className="slide-text">
+              La Ciudad de México es una de las principales productoras de Romeritos a nivel nacional, tan sólo el año pasado se obtuvieron mil 524 toneladas de las 225 hectáreas sembradas en en las alcaldías Xochimilco y Tláhuac. Entre ellas las que plantan año con año los hermanos Peña Piña con su cooperativa "Productores Agrícolas Piña" que lleva este platillo hasta la mesa navideña y de año nuevo. 
+ 
+              </p>
             </div>
           </div>
 
-          {/* Slide 3 - IMAGEN LATERAL DERECHA*/}
-          <div className="carousel-item">
+          {/* Slide 3 - Imagen lateral derecha */}
+          <div className="carousel-item carousel-right">
             <div className="slide-content slide-bg-right">
               <img
-                src="https://cdn3.pixelcut.app/7/20/uncrop_hero_bdf08a8ca6.jpg"
-                className="carousel-image small"
+                src="./public/images/chinampa1.jpg"
+                className="carousel-image"
                 alt="Imagen lateral derecha"
               />
-              <p className="slide-text small-text">Otra historia interesante...</p>
+              <a className="subtitulo" href="https://www.meteored.mx/noticias/actualidad/chinampas-el-ejemplo-de-agricultura-sostenible-que-ha-perdurado-durante-siglos-cultura-historia.html"><h3 className="subtitulo">Chinampas: el ejemplo de agricultura sostenible que ha perdurado durante siglos</h3></a>
+              <p className="slide-text ">Las chinampas son un antiguo sistema de cultivo desarrollado por los mexicas en el Valle de México. Este método consiste en crear islas flotantes sobre cuerpos de agua, aprovechando los recursos naturales del entorno para la producción de alimentos. Aunque su origen se remonta a tiempos prehispánicos, las chinampas siguen siendo una práctica agrícola vigente y relevante en la actualidad, por ejemplo en Tláhuac y Xochimilco.</p>
             </div>
           </div>
         </div>
