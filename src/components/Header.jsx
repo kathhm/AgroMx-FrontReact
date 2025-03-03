ab82d87e41c5a50b1483c8a41bb0d3190c46f5d2
Jael Bohorquez <Jaelbkz@icloud.com>
2025-03-02 23:19:58 -0600

    fix: no encontraba la ruta al usar google

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,65 +1,75 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import { NavLink } from "react-router-dom";
 
+
 function Header() {
+	const { user } = useContext(UserContext);
+
 	return (
-		<>
-			<div className="container-fluid">
-				{/* <!-- Barra superior con el logo, búsqueda y los iconos --> */}
-				<div className="row align-items-center">
-					{/* <!-- Logo --> */}
-					<div className="col-3 col-md-2 d-flex justify-content-center">
-						<img
-							className="img-fluid p-1"
-							id="logo"
-							src="/images/logVectorizado2.svg"
-							alt="Logo AgroMX"
-						/>
+		<div className="container-fluid">
+			<div className="row align-items-center">
+				<div className="col-3 col-md-2 d-flex justify-content-center">
+					<img
+						className="img-fluid p-1"
+						id="logo"
+						src="/images/logVectorizado2.svg"
+						alt="Logo AgroMX"
+					/>
+				</div>
+
+				<div className="col-5 col-md-6 d-flex justify-content-center align-items-center">
+					<div className="d-flex justify-content-center align-items-center me-3">
+						<button
+							id="lupa"
+							className="btn btn-outline-success btn-lupa d-none d-sm-block d-flex justify-content-center align-items-center"
+							type="submit"
+						>
+							<i className="bi bi-search"></i>
+						</button>
 					</div>
 
-					{/* <!-- Lupa y barra de búsqueda --> */}
-					<div className="col-5 col-md-6 d-flex justify-content-center align-items-center">
-						{/* <!-- Botón de búsqueda (Lupa) --> */}
-						<div className="d-flex justify-content-center align-items-center me-3">
-							<button
-								id="lupa"
-								className="btn btn-outline-success btn-lupa d-none d-sm-block"
-								type="submit"
-							>
-								<i className="bi bi-search"></i>
-							</button>
-						</div>
-
-						{/* <!-- Barra de búsqueda --> */}
-						<div className="d-flex justify-content-center w-100">
-							<form className="d-flex w-100" role="search">
-								<input
-									className="form-control me-2 w-100 barra-busqueda d-none d-sm-block"
-									type="search"
-									placeholder="Busca tu producto"
-									aria-label="Search"
-								/>
-							</form>
-						</div>
+					<div className="d-flex justify-content-center w-100">
+						<form className="d-flex w-100" role="search">
+							<input
+								className="form-control me-2 w-100 barra-busqueda d-none d-sm-block"
+								type="search"
+								placeholder="Busca tu producto"
+								aria-label="Search"
+							/>
+						</form>
 					</div>
+				</div>
 
-					{/* <!-- Carrito y Avatar --> */}
-					<div className="col-4 col-md-4 d-flex justify-content-end align-items-end mt-2 p-4 mt-md-0">
-						<i className="bi bi-basket2 mx-5">
+				<div className="col-4 col-md-4 d-flex justify-content-end align-items-end mt-2 p-4 mt-md-0">
+					<i className="bi bi-basket2 mx-5">
 						<NavLink
 							className="nav-link active"
 							aria-current="page"
 							to={"/Carrito"}
-						>
-						</NavLink>
-						</i>
+						></NavLink>
+					</i>
+					{user ? (
+						<img
+							src={user.picture}
+							alt="Foto de perfil"
+							className="rounded-circle"
+							width="40"
+							height="40"
+						/>
+					) : (
 						<i className="bi bi-person-circle me-4"></i>
-					</div>
+					)}
 				</div>
 			</div>
 
 			<nav className="navbar navbar-expand-lg navbar-light">
 				<div className="container-fluid">
-					<NavLink className="navbar-brand" id="linkToPage" to={"/Inicio"}>
+					<NavLink
+						className="navbar-brand"
+						id="linkToPage"
+						to={"/Inicio"}
+					>
 						Inicio
 					</NavLink>
 					<button
@@ -100,7 +110,7 @@ function Header() {
 							<li className="nav-item dropdown">
 								<NavLink
 									className="nav-link dropdown-toggle"
-									id = "linkToPage"
+									id="linkToPage"
 									to={"/Comunidad"}
 									role="button"
 									data-bs-toggle="dropdown"
@@ -136,7 +146,7 @@ function Header() {
 								<NavLink
 									className="nav-link"
 									id="linkToPage"
-									to={"/Registro"}	
+									to={"/Registro"}
 								>
 									Regístrate
 								</NavLink>
@@ -145,7 +155,7 @@ function Header() {
 					</div>
 				</div>
 			</nav>
-		</>
+		</div>
 	);
 }
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,16 @@
 import "./login.css";
 import {jwtDecode} from 'jwt-decode';
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
 
 function Login() {
-	const handleGoogleResponse = (response) => {
+  const { setUser } = useContext(UserContext);
+
+  const handleGoogleResponse = (response) => {
     const token = response.credential;
     const decodedToken = jwtDecode(token);
     console.log(decodedToken);
+    setUser(decodedToken);
     // Aquí puedes manejar la información del usuario decodificada
   };
 
@@ -15,68 +19,65 @@ function Login() {
   }, []);
 
   return (
-		<main className="container">
-			<div className="container login-card my-5 p-5">
-				<h1 className="titulo text-center">Inicia sesión</h1> <hr />
-				<div className="container-fluid justify-content-center d-flex">
-					<form className="form col-8 row">
-						<label
-							className="etiqueta me-2 form-label text-align-start"
-							htmlFor="correo"
-						>
-							Correo electronico:
-						</label>
-						<input
-							className="form-control"
-							type="text"
-							placeholder="example@dominio.com"
-							id="correo"
-						/>
-						<br />
-						<label
-							className="etiqueta me-2 form-label text-align-start"
-							htmlFor="contraseña"
-						>
-							Contraseña:
-						</label>
-						<input
-							className="form-control"
-							type="password"
-							placeholder="Introduce tu contraseña"
-							id="contraseña"
-						/>
-						<button
-							className="btn btn-primary w-100 addToCartBtn mt-3"
-							type="submit"
-						>
-							iniciar sesión
-						</button>
-					</form>
-				</div>
-        <div className="container-fluid mt-4">
-					<div
-						id="g_id_onload"
-						data-client_id="852189199431-elq37edvmgere2fgboo0gemqrouggcif.apps.googleusercontent.com"
-						data-login_uri="" //TODO: Agregar la URL de Backend para manejar el JWt del login con Google
-            data-callback="handleGoogleResponse"
-					></div>
-					<div
-						className="g_id_signin justify-content-center d-flex"
-						data-type="standard"
-						data-size="large"
-						data-theme="outline"
-						data-text="sign_up_with"
-						data-shape="circle"
-						data-logo_alignment="left"
-					></div>
+    <main className="container">
+      <div className="container login-card my-5 p-5">
+        <h1 className="titulo text-center">Inicia sesión</h1> <hr />
+        <div className="container-fluid justify-content-center d-flex">
+          <form className="form col-8 row">
+            <label
+              className="etiqueta me-2 form-label text-align-start"
+              htmlFor="correo"
+            >
+              Correo electronico:
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="example@dominio.com"
+              id="correo"
+            />
+            <br />
+            <label
+              className="etiqueta me-2 form-label text-align-start"
+              htmlFor="contraseña"
+            >
+              Contraseña:
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Introduce tu contraseña"
+              id="contraseña"
+            />
+            <button
+              className="btn btn-primary w-100 addToCartBtn mt-3"
+              type="submit"
+            >
+              iniciar sesión
+            </button>
+          </form>
         </div>
-			</div>
-          <script src="https://accounts.google.com/gsi/client" async></script>
-          
-		</main>
-	);
+        <div className="container-fluid mt-4">
+          <div
+            id="g_id_onload"
+            data-client_id="852189199431-elq37edvmgere2fgboo0gemqrouggcif.apps.googleusercontent.com"
+            data-login_uri="" //TODO: Agregar la URL de Backend para manejar el JWt del login con Google
+            data-callback="handleGoogleResponse"
+          ></div>
+          <div
+            className="g_id_signin justify-content-center d-flex"
+            data-type="standard"
+            data-size="large"
+            data-theme="outline"
+            data-text="sign_up_with"
+            data-shape="circle"
+            data-logo_alignment="left"
+          ></div>
+        </div>
+      </div>
+      <script src="https://accounts.google.com/gsi/client" async></script>
+    </main>
+  );
 }
 
-export default Login ;
-
-
+export default Login;
