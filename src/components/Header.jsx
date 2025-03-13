d80bcf8e8c32623a9c6af2309f576572dabb01d3
Katherine Hinojosa <geofisikatt@gmail.com>
2025-03-12 22:36:27 -0600

    navlink a iconos de carrito y usuario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,15 +50,20 @@ function Header() {
 					{/* <!-- Carrito y Avatar --> */}
 					<div className="col-4 col-md-4 d-flex justify-content-end align-items-end mt-2 p-4 mt-md-0">
 					<NavLink to={"/Carrito"} className="position-relative">
+					
 						<i className="bi bi-basket2 mx-5 position-relative"></i>
+						<p style={{fontSize:"0.75rem", textAlign:"center"}}>Carrito</p>
 						{countProducts > 0 && (
 							<span className="position-absolute badge rounded-pill bg-danger"
-								style={{ fontSize: "0.75rem", transform: "translate(-50%, -50%)" }}>
+							style={{ fontSize: "0.75rem", top: "-5px", right: "50px", transform: "translate(50%, -50%)" }}>
 								{countProducts}
 							</span>
 						)}
 					</NavLink>
-						<i className="bi bi-person-circle me-4"></i>
+					<NavLink to={"/Login"}>
+						<i className="bi bi-person-circle me-4 mx-4"></i>
+						<p style={{fontSize:"0.75rem", textAlign:"center"}}>Iniciar sesión</p>
+					</NavLink>
 					</div>
 				</div>
 			</div>
